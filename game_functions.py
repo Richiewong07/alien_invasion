49a499aa4dd97265a071cc6442ed3c462df46477
Richie Wong <richiewong07@yahoo.com>
2018-04-30 19:29:50 -0500

    updated

--- game_functions.py
+++ game_functions.py
@@ -5,28 +5,37 @@ import pygame
 def check_events(ship):
     """Respond to keypresses and mouse events."""
 
-
-
     for event in pygame.event.get(): # ACCESSES EVENT
         if event.type == pygame.QUIT:   # EVENT TO CLOSE SYSTEM
             sys.exit()
 
+        # RESPONSES TO KEYDOWN EVENT
+        elif event.type == pygame.KEYDOWN:
+            check_key_down_events(event,ship)
 
-        elif event.type == pygame.KEYDOWN:  # RESPONSES TO KEYDOWN EVENT
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_key_up_events(event,ship)
 
 
+def check_key_down_events(event, ship):
+    """ Respond to keypresses."""
+
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
 
 
+def check_key_up_events(event, ship):
+    """ Respond to keypresses."""
+
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+            ship.moving_left = False
+
+            
 def update_screen(ai_settings, screen, ship):
     """Update images on screen and flip to the new screen."""
 
